aadf2ee544cee864383cf4f3150772551a28b745
ehalfman <halfman@wisc.edu>
2020-02-09 11:43:48 -0600

    converter finished. no emulator

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,9 +2,25 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+               String str = ((EditText) findViewById(R.id.myTextField)).getText().toString();
+               // Toast.makeText(MainActivity.this,((EditText) findViewById(R.id.myTextField)).getText().toString(), Toast.LENGTH_LONG).show();
+    goToActivity2(str);
+    }
+
+    public void goToActivity2(String s){
+       Intent intent = new Intent(this, Main2Activity.class);
+       intent.putExtra("message", s);
+       startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
